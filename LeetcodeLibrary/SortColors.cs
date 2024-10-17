4f2e3e07a5c4f2ef6294c107cd32125be2b64f35
Sviatoslav Samoilenko <Sviatoslav.Samoilenko@toluna.com>
2024-10-18 00:21:52 +0200

    Added SortColors

---
+++ LeetcodeLibrary/SortColors.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetcodeLibrary
+{
+    public partial class LeetcodeTasks
+    {
+        //https://leetcode.com/problems/sort-colors/
+        public static void SortColors(int[] nums)
+        {
+            short[] items = new short[3];
+            for(short i = 0;i < nums.Length; ++i)
+            {
+                ++items[nums[i]];
+            }
+
+            byte j = 0;
+            for(short i = 0; i < nums.Length;)
+            {
+                while (items[j]-- > 0)
+                {
+                    nums[i++] = j;
+                }
+
+                ++j;
+            }
+        }
+    }
+}
