5f193b2a5eb5af668628bff7b82e5347bfd16e3b
Sviatoslav Samoilenko <Sviatoslav.Samoilenko@toluna.com>
2024-10-18 16:28:35 +0200

    Added TwoSum

---
+++ LeetcodeLibrary/TwoSum.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace LeetcodeLibrary
+{
+    public partial class LeetcodeTasks
+    {
+        //https://neetcode.io/problems/two-integer-sum
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> items = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; ++i)
+            {
+                if (!items.ContainsKey(nums[i]))
+                {
+                    items.Add(nums[i], i);
+                    var remainPart = target - nums[i];
+                    if (items.ContainsKey(remainPart) && items[remainPart] != i)
+                    {
+                        return new int[] { items[remainPart], i };
+                    }
+                }
+                else
+                {
+                    if (target == nums[i] * 2)
+                    {
+                        return new int[] { items[nums[i]], i };
+                    }
+                }
+            }
+
+            return new int[] { };
+        }
+    }
+}
---
+++ LeetcodeTests/TwoSum_Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    public class TwoSum_Tests
+    {
+        [Theory]
+        [TestCase(new int[] { 3, 4, 5, 6 }, 7, new int[] { 0, 1 })]
+        [TestCase(new int[] { 4, 5, 6 }, 10, new int[] { 0, 2 })]
+        [TestCase(new int[] { 5, 5 }, 10, new int[] { 0, 1 })]
+        public void TwoSum_TestCases(int[] input, int target, int[] expected)
+        {
+            var actual = LeetcodeLibrary.LeetcodeTasks.TwoSum(input, target);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
