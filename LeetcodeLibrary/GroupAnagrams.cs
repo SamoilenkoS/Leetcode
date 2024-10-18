2d94d3b223eb4786115ac2e5f87d6009addf91a9
Sviatoslav Samoilenko <Sviatoslav.Samoilenko@toluna.com>
2024-10-18 17:32:16 +0200

    Added GroupAnagrams

---
+++ LeetcodeLibrary/GroupAnagrams.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetcodeLibrary
+{
+    public partial class LeetcodeTasks
+    {
+        public static List<List<string>> GroupAnagrams(string[] strs)
+        {
+            Dictionary<string, List<string>> anagramsHelper = new Dictionary<string, List<string>>();
+            foreach (string str in strs)
+            {
+                var sorted = GetSortedString(str);
+                if (!anagramsHelper.ContainsKey(sorted))
+                {
+                    anagramsHelper.Add(sorted, new List<string> { str });
+                }
+                else
+                {
+                    anagramsHelper[sorted].Add(str);
+                }
+            }
+
+            return anagramsHelper.Select(x => x.Value).ToList();
+        }
+
+        private static string GetSortedString(string str)
+        {
+            char[] charArray = str.ToCharArray();
+            Array.Sort(charArray);
+            return new string(charArray);
+        }
+    }
+}
---
+++ LeetcodeTests/GroupAnagrams_Tests.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using System.Linq;
+
+namespace LeetcodeTests
+{
+    public class GroupAnagrams_Tests
+    {
+        public static IEnumerable Anagrams_TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(
+                    new string[] { "act", "pots", "tops", "cat", "stop", "hat" },
+                    new string[][] {
+                        new string[] { "hat" },
+                        new string[] { "act", "cat" },
+                        new string[] { "stop", "pots", "tops" }
+                    });
+
+                yield return new TestCaseData(
+                     new string[] { "x" },
+                     new string[][] {
+                        new string[] { "x" }
+                    });
+
+                yield return new TestCaseData(
+                     new string[] { "" },
+                     new string[][] {
+                        new string[] { "" }
+                    });
+            }
+        }
+
+        [TestCaseSource(nameof(Anagrams_TestCases))]
+        public void GroupAnagrams_TestCases(string[] input, string[][] expected)
+        {
+            var actual = LeetcodeLibrary.LeetcodeTasks.GroupAnagrams(input);
+
+            Assert.That(actual.Count, Is.EqualTo(expected.Length));
+
+            var expectedDict = expected
+            .Select(seq => string.Join(",", seq.OrderBy(x => x)))
+            .GroupBy(seq => seq)
+            .ToDictionary(group => group.Key, group => group.Count());
+
+            var actualDict = actual
+                .Select(seq => string.Join(",", seq.OrderBy(x => x)))
+                .GroupBy(seq => seq)
+                .ToDictionary(group => group.Key, group => group.Count());
+
+            // Check that both dictionaries are equal
+            Assert.That(actualDict, Is.EqualTo(expectedDict));
+        }
+    }
+}
