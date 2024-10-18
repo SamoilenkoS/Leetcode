d63dfb3293aa1325c3707428f7decfd5be5946a7
Sviatoslav Samoilenko <Sviatoslav.Samoilenko@toluna.com>
2024-10-18 18:44:04 +0200

    Added TopKFrequent

---
+++ LeetcodeLibrary/TopKFrequent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+
+namespace LeetcodeLibrary
+{
+    public partial class LeetcodeTasks
+    {
+        public static int[] TopKFrequent(int[] nums, int k)
+        {
+            Dictionary<int, int> map = new Dictionary<int, int>();
+            foreach(var i in nums)
+            {
+                if (map.ContainsKey(i))
+                {
+                    ++map[i];
+                }
+                else
+                {
+                    map.Add(i, 1);
+                }
+            }
+
+            return map.OrderByDescending(x => x.Value).Select(x => x.Key).Take(k).ToArray();
+        }
+    }
+}
---
+++ LeetcodeTests/TopKFrequent_Tests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    public class TopKFrequent_Tests
+    {
+        [Theory]
+        [TestCase(new int[] { 1, 2, 2, 3, 3, 3 }, 2, new int[] { 2, 3 })]
+        [TestCase(new int[] { 7, 7 }, 1, new int[] { 7 })]
+        public void TopKFrequent_TestCases(int[] input, int k, int[] expected)
+        {
+            var actual = LeetcodeLibrary.LeetcodeTasks.TopKFrequent(input, k);
+
+            CollectionAssert.AreEquivalent(expected, actual);
+        }
+    }
+}
