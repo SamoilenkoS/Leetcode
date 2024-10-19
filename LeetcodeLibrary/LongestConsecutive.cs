2e2b449d2c8f0e900ae8a638f68819603cb64ed5
Sviatoslav Samoilenko <Sviatoslav.Samoilenko@toluna.com>
2024-10-19 20:45:16 +0200

    Added LongestConsecutive

---
+++ LeetcodeLibrary/LongestConsecutive.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace LeetcodeLibrary
+{
+    public partial class LeetcodeTasks
+    {
+        public static int LongestConsecutive(int[] nums)
+        {
+            var set = new SortedSet<int>(nums);
+
+            int longest = 0 ;
+            int current = 0;
+            int previousNum = int.MinValue;
+            foreach(var x in set)
+            {
+                if((x - previousNum) == 1)
+                {
+                    current++;
+                }
+                else
+                {
+                    if (current > longest)
+                    {
+                        longest = current;
+                    }
+
+                    current = 1;
+                }
+
+                previousNum = x;
+            }
+
+            if (current > longest)
+            {
+                longest = current;
+            }
+
+            return longest;
+        }
+    }
+}
---
+++ LeetcodeTests/LongestConsecutive_Tests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    public class LongestConsecutive_Tests
+    {
+        [Theory]
+        [TestCase(new int[] { 100, 4, 200, 1, 3, 2 }, 4)]
+        [TestCase(new int[] { 0, 3, 7, 2, 5, 8, 4, 6, 0, 1 }, 9)]
+        public void LongestConsecutive(int[] input, int expected)
+        {
+            var actual = LeetcodeLibrary.LeetcodeTasks.LongestConsecutive(input);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
