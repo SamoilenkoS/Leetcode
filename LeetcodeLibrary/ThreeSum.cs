65603823ef595709ddf5f9c521dce544b73d89ac
Sviatoslav Samoilenko <Sviatoslav.Samoilenko@toluna.com>
2024-10-24 01:01:20 +0200

    Added ThreeSum (v1) 308\313

---
+++ LeetcodeLibrary/ThreeSum.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetcodeLibrary
+{
+    public partial class LeetcodeTasks
+    {
+        private static Tuple<int, int, int> GetTuple(int a, int b, int c)
+        {
+            int min, mid, max;
+
+            if (a <= b && a <= c)
+            {
+                min = a;
+            }
+            else if (b <= a && b <= c)
+            {
+                min = b;
+            }
+            else
+            {
+                min = c;
+            }
+
+            if (a >= b && a >= c)
+            {
+                max = a;
+            }
+            else if (b >= a && b >= c)
+            {
+                max = b;
+            }
+            else
+            {
+                max = c;
+            }
+
+            mid = a + b + c - min - max;
+
+            return Tuple.Create(min, mid, max);
+        }
+
+        public static IList<IList<int>> ThreeSum(int[] nums)
+        {
+            IList<IList<int>> response = new List<IList<int>>();
+            Dictionary<int, HashSet<Tuple<short, short>>> pairs =
+                new Dictionary<int, HashSet<Tuple<short, short>>>();
+
+            for (short i = 0; i < nums.Length - 1; ++i)
+            {
+                for (short j = (short)(i + 1); j < nums.Length; ++j)
+                {
+                    var currentSum = nums[i] + nums[j];
+                    var tuple = new Tuple<short, short>(i, j);
+                    if (!pairs.ContainsKey(currentSum))
+                    {
+                        pairs.Add(currentSum, new HashSet<Tuple<short,short>> { tuple });
+                    }
+                    else
+                    {
+                        pairs[currentSum].Add(tuple);
+                    }
+                }
+            }
+            HashSet<Tuple<int, int, int>> tempResponse = new HashSet<Tuple<int, int, int>>();
+            for (int i = 0; i < nums.Length; ++i)
+            {
+                if (pairs.ContainsKey(-nums[i]))
+                {
+                    var resultPairs = pairs[-nums[i]]
+                        .Where(x =>
+                            x.Item1 != i &&
+                            x.Item2 != i);
+                    if (resultPairs.Any())
+                    {
+                        foreach (var pair in resultPairs)
+                        {
+                            var tuple = GetTuple(pair.Item1, pair.Item2, i);
+                            tempResponse.Add(tuple);
+                        }
+                    }
+                }
+            }
+            var itemsTuple = new HashSet<Tuple<int, int, int>>();
+            foreach (var pair in tempResponse)
+            {
+                itemsTuple.Add(GetTuple(nums[pair.Item1], nums[pair.Item2], nums[pair.Item3]));
+            }
+
+            foreach (var tuple in itemsTuple)
+            {
+                response.Add(new List<int> { tuple.Item1, tuple.Item2, tuple.Item3 });
+            }
+
+            return response;
+        }
+    }
+}
---
+++ LeetcodeTests/ThreeSum_Tests.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using System.Linq;
+
+namespace LeetcodeTests
+{
+    public class ThreeSum_Tests
+    {
+        public static IEnumerable PositiveTestCases
+        {
+            get
+            {
+                yield return new TestCaseData(
+                    new int[] { -1, 0, 1, 2, -1, -4 },
+                    new int[][] { new int[] { -1, -1, 2 }, new int[] { -1, 0, 1 } });
+
+                yield return new TestCaseData(
+                    new int[] { 0, 0, 0 },
+                    new int[][] { new int[] { 0, 0, 0 } });
+
+                yield return new TestCaseData(
+                    new int[] { 0, 1, 1 },
+                    new int[][] { });
+            }
+        }
+
+        [TestCaseSource(nameof(PositiveTestCases))]
+        public void ThreeSum_TestCases(int[] input, int[][] expected)
+        {
+            var actual = LeetcodeLibrary.LeetcodeTasks.ThreeSum(input);
+            Assert.That(actual.Count, Is.EqualTo(expected.Length));
+
+            // Convert each sequence to a sorted version and use a dictionary to count occurrences
+            var expectedDict = expected
+                .Select(seq => string.Join(",", seq.OrderBy(x => x)))
+                .GroupBy(seq => seq)
+                .ToDictionary(group => group.Key, group => group.Count());
+
+            var actualDict = actual
+                .Select(seq => string.Join(",", seq.OrderBy(x => x)))
+                .GroupBy(seq => seq)
+                .ToDictionary(group => group.Key, group => group.Count());
+
+            // Check that both dictionaries are equal
+            Assert.That(actualDict, Is.EqualTo(expectedDict));
+        }
+    }
+}
