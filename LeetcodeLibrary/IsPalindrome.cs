fcbdb02063f2f99f598bf429a519aed231cb5d17
Sviatoslav Samoilenko <Sviatoslav.Samoilenko@toluna.com>
2024-10-23 22:40:49 +0200

    Added IsPalindrome

---
+++ LeetcodeLibrary/IsPalindrome.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetcodeLibrary
+{
+    public partial class LeetcodeTasks
+    {
+        public static bool IsPalindrome(string s)
+        {
+            for (int i = 0, j = s.Length - 1; i < s.Length && j >= 0; ++i, --j)
+            {
+                while (i < s.Length && !char.IsLetter(s[i]) && !char.IsDigit(s[i]))
+                {
+                    ++i;
+                }
+                while (j > 0 && !char.IsLetter(s[j]) && !char.IsDigit(s[j]))
+                {
+                    --j;
+                }
+                if (i < s.Length && j > 0 && char.ToLower(s[i]) != char.ToLower(s[j]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ LeetcodeTests/IsPalindrome_Tests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    public class IsPalindrome_Tests
+    {
+        [Theory]
+        [TestCase("Was it a car or a cat I saw?", true)]
+        [TestCase("tab a cat", false)]
+        [TestCase(" ", true)]
+        [TestCase("0P", false)]
+        [TestCase(".,", true)]
+        public void IsPalindrom_TestCases(string input, bool expected)
+        {
+            Assert.That(LeetcodeLibrary.LeetcodeTasks.IsPalindrome(input),
+                Is.EqualTo(expected));
+        }
+    }
+}
