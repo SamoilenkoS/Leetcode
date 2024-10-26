1e58675272c0b7215fa34b9754e5380cc8103282
Sviatoslav Samoilenko <Sviatoslav.Samoilenko@toluna.com>
2024-10-26 23:21:28 +0200

    Added MaxArea

---
+++ LeetcodeLibrary/MaxArea.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LeetcodeLibrary
+{
+    public partial class LeetcodeTasks
+    {
+        // https://leetcode.com/problems/container-with-most-water/
+        public static int MaxArea(int[] height)
+        {
+            int result = 0;
+            for (int i = 0, j = height.Length - 1; i < height.Length && j > i;)
+            {
+                var min = MinI(height, i, j);
+                var currentResult = (j - i) * height[min];
+                if (currentResult > result)
+                {
+                    result = currentResult;
+                }
+                if(min == i)
+                {
+                    i++;
+                }
+                else
+                {
+                    j--;
+                }
+            }
+
+            return result;
+        }
+
+        private static int MinI(int[] height, int i, int j)
+        {
+            return height[i] < height[j] ? i : j;
+        }
+    }
+}
---
+++ LeetcodeTests/MaxArea_Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    public class MaxArea_Tests
+    {
+        [Theory]
+        [TestCase(new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 }, 49)]
+        [TestCase(new int[] { 1, 1 }, 1)]
+        [TestCase(new int[] { 1, 7, 2, 5, 4, 7, 3, 6 }, 36)]
+        [TestCase(new int[] { 2, 2, 2 }, 4)]
+        [TestCase(new int[] { 1, 2, 4, 3, 1 }, 4)]
+        public void MaxArea_TestCases(int[] input, int expected)
+        {
+            Assert.That(LeetcodeLibrary.LeetcodeTasks.MaxArea(input), Is.EqualTo(expected));
+        }
+    }
+}
