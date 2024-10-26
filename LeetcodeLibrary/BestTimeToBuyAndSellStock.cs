5d5b64c3ad2451067cfb890eb46e23efbf73aa8e
Sviatoslav Samoilenko <Sviatoslav.Samoilenko@toluna.com>
2024-10-27 01:42:18 +0200

    Added BestTimeToBuyAndSellStock

---
+++ LeetcodeLibrary/BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetcodeLibrary
+{
+    public partial class LeetcodeTasks
+    {
+        //https://leetcode.com/problems/best-time-to-buy-and-sell-stock
+        public static int BestTimeToBuyAndSellStock(int[] prices)
+        {
+            int result = 0;
+            for (int i = 0, j = i + 1; j < prices.Length && i < prices.Length;)
+            {
+                var current = prices[j] - prices[i];
+                if (current > result)
+                {
+                    result = current;
+                }
+                if (prices[i] > prices[j])
+                {
+                    ++i;
+                }
+                else
+                {
+                    ++j;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetcodeTests/BestTimeToBuyAndSellStock_Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    public class BestTimeToBuyAndSellStock_Tests
+    {
+        [Theory]
+        [TestCase(new int[] { 7, 1, 5, 3, 6, 4 }, 5)]
+        [TestCase(new int[] { 7, 6, 4, 3, 1 }, 0)]
+        [TestCase(new int[] { 10, 1, 5, 6, 7, 1 }, 6)]
+        [TestCase(new int[] { 5, 1, 5, 6, 7, 1, 10 }, 9)]
+        [TestCase(new int[] { 2, 1, 4 }, 3)]
+        public void BestTimeToBuyAndSellStock_TestCases(int[] input, int expected)
+        {
+            Assert.That(LeetcodeLibrary.LeetcodeTasks.BestTimeToBuyAndSellStock(input), Is.EqualTo(expected));
+        }
+    }
+}
