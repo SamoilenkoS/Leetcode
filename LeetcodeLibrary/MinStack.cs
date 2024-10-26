e550184e8aa79f3ab4675301c58a32fe582a7694
Sviatoslav Samoilenko <Sviatoslav.Samoilenko@toluna.com>
2024-10-27 00:38:21 +0200

    Added MinStack

---
+++ LeetcodeLibrary/MinStack.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace LeetcodeLibrary
+{
+    public partial class LeetcodeTasks
+    {
+        //https://leetcode.com/problems/min-stack/
+        public class MinStack
+        {
+            private class Node
+            {
+                public Node(int val)
+                {
+                    Value = val;
+                }
+                public Node Next { get; set; }
+                public int Value { get; set; }
+            }
+
+            private Node _minRoot;
+            private Node _root;
+            public MinStack()
+            {
+
+            }
+
+            public void Push(int val)
+            {
+                Node newRoot = new Node(val);
+                newRoot.Next = _root;
+                _root = newRoot;
+                int currentMinRootValue = _minRoot != null ? GetMin() : val;
+                Node newMinRoot = new Node(Math.Min(val, currentMinRootValue));
+                newMinRoot.Next = _minRoot;
+                _minRoot = newMinRoot;
+            }
+
+            public void Pop()
+            {
+                _root = _root.Next;
+                _minRoot = _minRoot.Next;
+            }
+
+            public int Top()
+            {
+                return _root.Value;
+            }
+
+            public int GetMin()
+            {
+                return _minRoot.Value;
+            }
+        }
+    }
+}
