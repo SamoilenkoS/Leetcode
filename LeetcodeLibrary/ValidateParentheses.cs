ae225146df5257fb58f084d51a4c550e5242946a
Sviatoslav Samoilenko <Sviatoslav.Samoilenko@toluna.com>
2024-10-26 23:50:13 +0200

    Added ValidateParentheses

---
+++ LeetcodeLibrary/ValidateParentheses.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetcodeLibrary
+{
+    public partial class LeetcodeTasks
+    {
+        // https://leetcode.com/problems/valid-parentheses
+        public static bool ValidateParentheses(string s)
+        {
+            string left = "({[";
+            Stack<char> items = new Stack<char>();
+            foreach (char c in s)
+            {
+                if (left.Contains(c))
+                {
+                    items.Push(c);
+                }
+                else if (!items.Any() || GetPair(items.Peek()) != c)
+                {
+                    return false;
+                }
+                else
+                {
+                    items.Pop();
+                }
+            }
+
+            return !items.Any();
+        }
+
+        private static char GetPair(char a)
+        {
+            switch (a)
+            {
+                case '(':
+                    return ')';
+                case '{':
+                    return '}';
+                case '[':
+                    return ']';
+                default:
+                    throw new Exception();
+            }
+        }
+    }
+}
---
+++ LeetcodeTests/ValidateParentheses_Tests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    public class ValidateParentheses_Tests
+    {
+        [Theory]
+        [TestCase("()", true)]
+        [TestCase("()[]{}", true)]
+        [TestCase("(]", false)]
+        [TestCase("([])", true)]
+        [TestCase("([{}])", true)]
+        [TestCase("[(])", false)]
+        [TestCase("]", false)]
+        public void ValidateParentheses_TestCases(string input, bool expected)
+        {
+            Assert.That(expected, Is.EqualTo(LeetcodeLibrary.LeetcodeTasks.ValidateParentheses(input)));
+        }
+    }
+}
