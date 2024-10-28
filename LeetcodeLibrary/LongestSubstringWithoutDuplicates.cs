b4e1c772895df827084be370d5602a940fe90337
Sviatoslav Samoilenko <Sviatoslav.Samoilenko@toluna.com>
2024-10-28 18:39:43 +0100

    Added LongestSubstringWithoutDuplicates

---
+++ LeetcodeLibrary/LongestSubstringWithoutDuplicates.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetcodeLibrary
+{
+    public partial class LeetcodeTasks
+    {
+        //https://leetcode.com/problems/longest-substring-without-repeating-characters
+        public static int LongestSubstringWithoutDuplicates(string s)
+        {
+            int result = 0;
+            HashSet<char> set = new HashSet<char>();
+            for (int left = 0, right = 0; right < s.Length; ++right)
+            {
+                while (set.Contains(s[right]))
+                {
+                    set.Remove(s[left++]);
+                }
+
+                set.Add(s[right]);
+                result = Math.Max(result, right - left + 1);
+            }
+
+            return result;
+        }
+}
+}
---
+++ LeetcodeTests/LongestSubstringWithoutDuplicates_Tests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    public class LongestSubstringWithoutDuplicates_Tests
+    {
+        [Theory]
+        [TestCase("abcabcbb", 3)]
+        [TestCase("bbbbb", 1)]
+        [TestCase("pwwkew", 3)]
+        [TestCase("zxyzxyz", 3)]
+        [TestCase("xxxx", 1)]
+        [TestCase("dvdf", 3)]
+        public void LongestSubstringWithoutDuplicates_TestCases(string input, int expected)
+        {
+            Assert.That(
+                LeetcodeLibrary.LeetcodeTasks.LongestSubstringWithoutDuplicates(input), Is.EqualTo(expected));
+        }
+    }
+}
